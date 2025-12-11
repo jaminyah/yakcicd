250bd696b27c930d15e38f010b60daa3f6b611fe
Patrick <jaminyah@gmail.com>
2025-12-11 15:02:43 -0600

    changed app test demo.

--- app.js
+++ app.js
@@ -6,5 +6,5 @@
 
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello CI/CD!"));
+app.get("/", (req, res) => res.send("Hello GitHub Actions CI/CD!"));
 app.listen(3000, () => console.log("App running on port 3000"));
