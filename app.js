97a51930ff9598963900eeb3c6a3ceceb55c85a0
Patrick <jaminyah@gmail.com>
2025-12-28 05:42:00 -0600

    modify app.js to read html file.

--- app.js
+++ app.js
@@ -4,9 +4,18 @@
  * reference: https://medium.com/@kanishetty/how-to-build-a-ci-cd-pipeline-from-scratch-step-by-step-guide-b3913cf3d799
  */
 
-const express = require("express");
+const express = require('express');
+const { readFile } = require('fs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello GitHub Actions, Docker CI/CD!"));
+app.get('/', (req, res) => {
+  readFile('./index.html', 'utf-8', (err, html) => {
+      if(err){
+          res.send("service currenty unavailable");
+      }
+      res.send(html);
+  })
+})
+
 app.listen(port, () => console.log(`App running on port ${port}`));
