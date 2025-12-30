219b21a39394513e4f0cddf8fe3163654ddb5a57
Patrick <jaminyah@gmail.com>
2025-12-30 16:56:55 -0600

    modify actions update_ssh_linode to use ssh key.

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ function upperCase() {
     var input = document.getElementById("latin").innerHTML;
     input = input.toUpperCase();
     document.getElementById("latin").innerHTML = input;
-    document.getElementById("latin").style.color = '#0000FF';
+    document.getElementById("latin").style.color = '#088030ff';
 }
