5fd9d07e15d462daba578814e12cef2000a69585
Patrick <jaminyah@gmail.com>
2025-12-30 18:21:47 -0600

    change uppercase text color.

--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ function upperCase() {
     var input = document.getElementById("latin").innerHTML;
     input = input.toUpperCase();
     document.getElementById("latin").innerHTML = input;
-    document.getElementById("latin").style.color = '#088030ff';
+    document.getElementById("latin").style.color = '#6c0880ff';
 }
