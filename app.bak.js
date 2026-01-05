214f55e718d707ecdff994ee52da69e6cc115e60
Patrick <jaminyah@gmail.com>
2026-01-05 13:22:04 -0600

    add https secure domain.

--- app.bak.js
+++ app.bak.js
@@ -4,26 +4,13 @@
  * npm install express
  * reference: 
  * https://medium.com/@kanishetty/how-to-build-a-ci-cd-pipeline-from-scratch-step-by-step-guide-b3913cf3d799
- * 
- * Ref: https://stackoverflow.com/questions/11744975/enabling-https-on-express-js
- * create selfsigned.key and selfsigned.crt files at the bash shell:
- * openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./selfsigned.key -out selfsigned.crt
  */
 
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const app = express();
 
 // map localhost to docker container port
 const port = process.env.PORT || 3000;
-
-var key = fs.readFileSync(__dirname + '/certs/selfsigned.key');
-var cert = fs.readFileSync(__dirname + '/certs/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
  
 // Serve static files from the 'public' directory
 app.use(express.static(__dirname + '/public'));
@@ -41,9 +28,7 @@ app.use((err, req, res, next) => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
-
-var server = https.createServer(options, app);
-
-server.listen(port, () => {
-  console.log("server starting on port : " + port)
+ 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
